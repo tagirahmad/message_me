833fbf1ce228b42ca524eeb687f168426e01f31c
tagirahmad <tagirbpl@gmail.com>
2021-01-11 13:05:22 +0300

    add favicon

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,13 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
+//= require rails-ujs 
+//= require jquery
 //= require activestorage
 //= require turbolinks
+//= require semantic-ui
 //= require_tree .
+
+$(document).on('turbolinks:load', function () {
+    $('.ui.dropdown').dropdown();
+})
