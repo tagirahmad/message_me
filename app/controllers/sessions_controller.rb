48ae7bb91cb48e6fe01bf3633cfeb0fbc3369ebf
tagirahmad <tagirbpl@gmail.com>
2021-01-12 09:55:23 +0300

    Lot of fixes and a lot of new stuff

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,31 @@
 class SessionsController < ApplicationController
+  before_action :logged_in_redirect, only: %i[new create]
   def new; end
+
+  def create
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = 'You have successfully logged in'
+      redirect_to root_path
+    else
+      flash[:error] = 'Username or password is wrong'
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = 'You have successfully logged out'
+    redirect_to login_path
+  end
+
+  private
+
+  def logged_in_redirect
+    return unless logged_in?
+
+    flash[:error] = 'You have already logged in'
+    redirect_to root_path
+  end
 end
