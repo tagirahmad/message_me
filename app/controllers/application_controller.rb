39454113a2efc49078d772dcdd045c027e712715
tagirahmad <tagirbpl@gmail.com>
2021-01-13 19:10:36 +0300

    lot of changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,6 @@ class ApplicationController < ActionController::Base
     return if logged_in?
 
     flash[:error] = 'You must be logged in to perform that action!'
-    redirec_to login_path
+    redirect_to login_path
   end
 end
