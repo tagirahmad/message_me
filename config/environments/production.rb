704e3a9d15a0190f53ab1bdefe16a0a2da679b66
tagirahmad <tagirbpl@gmail.com>
2021-01-13 20:53:21 +0300

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,4 +93,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.web_socket_server_url = 'wss://message-me-coffe-chat.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = ['https://message-me-coffe-chat.herokuapp.com', 'http://message-me-coffe-chat.herokuapp.com']
 end
